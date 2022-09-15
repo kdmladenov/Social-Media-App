4bc7e2dc60364ab07a7dfba5ee3c6f9fad71d9a7
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-09-15 22:42:54 +0300

    Photolist - paging, controls, modal added

--- frontend/src/pages/profile/PhotoList.tsx
+++ frontend/src/pages/profile/PhotoList.tsx
@@ -1,39 +1,86 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/Button';
-import PostImageType from '../../types/PostImageType';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import HeaderControls from '../../components/HeaderControls';
+import Modal from '../../components/Modal';
+import Pagination from '../../components/Pagination';
+import Slider from '../../components/Slider';
+import { listUserImages } from '../../context/actions/userActions';
+import { BASE_URL } from '../../data/constants';
+import defaultEndpoint from '../../data/inputs/defaultEndpoint';
+import { imagesListPageSizeOptionsMap } from '../../data/inputs/pageSizeOptionsMap';
+import { imagesListSortOptionsMap } from '../../data/inputs/sortDropdownOptionsMaps';
+import useTypedSelector from '../../hooks/useTypedSelector';
 
 import './styles/PhotoList.css';
 
-const PhotoList: React.FC<{ screen: string; photos: PostImageType[] }> = ({
-  screen = '',
-  photos
-}) => {
-  const navigate = useNavigate();
+const PhotoList: React.FC<{ screen: string }> = ({ screen = '' }) => {
+  const dispatch = useDispatch();
+  const [endpoint, setEndpoint] = useState({
+    ...defaultEndpoint['photoList'],
+    pageSize: screen === 'profile_posts_screen' ? 'pageSize=9&' : 'pageSize=12&'
+  });
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(<></>);
+      const [selectedPostImageIndex, setSelectedPostImageIndex] = useState(0);
 
-  const photoCount =
-    screen === 'profile_posts_screen' ? 9 : 'profile_about_screen' ? 12 : photos.length;
+    
+
+  const { userImages } = useTypedSelector((state) => state.userImagesList);
+
+      const imageModalHandler = (imageId: number) => {
+        setSelectedPostImageIndex(userImages?.findIndex((image) => image.imageId === imageId));
+        setIsModalOpen(true);
+      };
+
+  useEffect(() => {
+    const { page, pageSize, sort, search } = endpoint;
+    dispatch(listUserImages(`${page}${pageSize}${sort}${search}`));
+  }, [dispatch, endpoint]);
 
   return (
     <div className={`photo_list card flex_col ${screen}`}>
       <div className="header flex">
-        <h1 onClick={() => navigate('/profile/photos')}>Photos</h1>
-        <Button classes="text" onClick={() => navigate('/profile/photos')}>
-          {screen === 'profile_posts_screen' ? ' See all photos' : 'Add Photos'}
-        </Button>
+        <h1>Photos</h1>
+        {screen === 'profile_photos_screen' && (
+          <HeaderControls
+            updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+            query={endpoint}
+            pageSizeOptionsMap={imagesListPageSizeOptionsMap}
+            sortOptionsMap={imagesListSortOptionsMap}
+          />
+        )}
       </div>
       <div className="photo_list">
-        {photos.slice(0, photoCount).map((photo) => (
+        {userImages?.map((photo) => (
           <img
-            // src={image?.startsWith('http') ? image : `${BASE_URL}/${image}`}
-            src={photo.image}
-            alt={photo.image}
-            key={photo.postImageId}
-            
+            src={photo.image?.startsWith('http') ? photo.image : `${BASE_URL}/${photo.image}`}
+            alt={photo.image || 'image'}
+            crossOrigin="anonymous"
+            onClick={() => imageModalHandler(photo?.imageId)}
           />
         ))}
       </div>
-      Pagination
+      <Pagination
+        updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+        currentPage={+endpoint.page.slice('page='.length).replace('&', '')}
+        pageSize={+endpoint.pageSize.slice('pageSize='.length).replace('&', '')}
+        totalItems={userImages?.[0]?.totalDBItems}
+      />
+      {isModalOpen && (
+        <Modal classes="images_list" setIsOpenModal={setIsModalOpen}>
+          <div className="images_container">
+            <Slider
+              dots={false}
+              slideIndex={selectedPostImageIndex}
+              setSlideIndex={setSelectedPostImageIndex}
+            >
+              {userImages?.map((image) => (
+                <Slider.Item item={image} button_controls={true} />
+              ))}
+            </Slider>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };

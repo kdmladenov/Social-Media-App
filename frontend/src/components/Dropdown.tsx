0aa33b688bec9b4c8037764c842ef3d4ce5cba54
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-09-17 21:26:58 +0300

    Dropdown - conditional body pointer and upd button

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -6,18 +6,26 @@ import useOutsideClick from '../hooks/useOutsideClick';
 import Tooltip from './Tooltip';
 import DropDownProps from '../types/components/DropDownProps';
 
-const DropDown: React.FC<DropDownProps> = ({ button, tooltipText = '', children }) => {
+const DropDown: React.FC<DropDownProps> = ({
+  button,
+  tooltipText = '',
+  children,
+  isPointed = false
+}) => {
   const [showBody, setShowBody] = useState(false);
 
   let nodeRef: LegacyRef<HTMLDivElement> = useOutsideClick(() => setShowBody(false));
 
   return (
     <div className="dropdown" ref={nodeRef}>
-      <div onClick={() => setShowBody(!showBody)}>
-        <Tooltip text={tooltipText}>{button}</Tooltip>
+      <div onClick={() => setShowBody(!showBody)} className="header flex">
+        {tooltipText ? <Tooltip text={tooltipText}>{button}</Tooltip> : button}
       </div>
 
-      <div onClick={() => setShowBody(false)} className={`body ${showBody ? 'show' : ''}`}>
+      <div
+        onClick={() => setShowBody(false)}
+        className={`body ${showBody ? 'show' : ''} ${isPointed ? 'pointed' : 'not_pointed'}`}
+      >
         {children}
       </div>
     </div>

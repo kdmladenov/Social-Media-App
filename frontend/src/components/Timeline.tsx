bdbd2b32fe90ea2aaff1d46873d58190ffe46418
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-09-30 22:02:38 +0300

    Timeline - button and text as props

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -1,46 +1,25 @@
 import React, { useState } from 'react';
 
 import './styles/Timeline.css';
-import getDate from '../utils/getDate';
-
-import Tooltip from './Tooltip';
 import TimelineType, { TimelineItemType } from '../types/TimelineType';
 
-const Timeline: TimelineType = ({ horizontal, children }) => {
+const Timeline: TimelineType = ({ horizontal = false, children }) => {
   return <ul className={`timeline_container ${horizontal ? 'horizontal' : ''}`}>{children}</ul>;
 };
 
-const Item: TimelineItemType = ({ historyRecord, deleteHistoryItem, children }) => {
-  const [removeMode, setRemoveMode] = useState(false);
+const Item: TimelineItemType = ({ children, button, text, hoverText }) => {
+  const [showButton, setShowButton] = useState(false);
 
   return (
     <li
       className="timeline_item"
-      onMouseEnter={() => setRemoveMode(true)}
-      onMouseLeave={() => setRemoveMode(false)}
+      onMouseEnter={() => setShowButton(true)}
+      onMouseLeave={() => setShowButton(false)}
     >
       <div className="timeline_item_content">
         {children}
-        {removeMode ? (
-          <span className="remove_btn" onClick={() => deleteHistoryItem(historyRecord.postId)}>
-            <Tooltip text="Remove">
-              <i className="fa fa-times" />
-            </Tooltip>
-          </span>
-        ) : (
-          <span className="point" />
-        )}
-        {removeMode ? (
-          <span className="date">Remove</span>
-        ) : (
-          <span className="date">
-            {getDate(historyRecord.dateVisited, 0, false)
-              .split(',')
-              .map((subString, index) => (
-                <span key={index}>{subString}</span>
-              ))}
-          </span>
-        )}
+        {showButton ? button : <span className="point" />}
+        {<span className="date">{showButton ? hoverText : text}</span>}
       </div>
     </li>
   );
